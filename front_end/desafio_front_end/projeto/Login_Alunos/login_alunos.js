23b5e858694a791b778aab8b1d532639aaa59c52
Cristian Massaneiro <cristianworc@gmail.com>
2024-03-24 11:33:21 -0300

    add codes

--- front_end/desafio_front_end/projeto/Login_Alunos/login_alunos.js
+++ front_end/desafio_front_end/projeto/Login_Alunos/login_alunos.js
@@ -1,14 +1,30 @@
-document.getElementById("login-form").addEventListener("submit", function(event){
-    event.preventDefault();
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+            
+    // Obtém os valores dos campos de entrada
+    var username = document.getElementById('signup-username').value;
+    var password = document.getElementById('signup-password').value;
 
-    // Aqui você pode adicionar a lógica para verificar o nome de usuário e senha, por exemplo:
-    if(username === "usuario" && password === "senha"){
-        alert("Login bem-sucedido!");
-        // Redirecionamento para outra página após o login bem-sucedido
-        // window.location.href = "pagina_de_redirecionamento.html";
-    } else {
-        alert("Nome de usuário ou senha incorretos.");
-    }
+    // Envia os dados para o backend
+    fetch('http://db-project-unisinos.c72kcyc8yf0o.us-east-1.rds.amazonaws.com/sua-rota-de-cadastro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Conta criada com sucesso!');
+            // Limpa os campos do formulário após o envio bem-sucedido
+            document.getElementById('signup-username').value = '';
+            document.getElementById('signup-password').value = '';
+        } else {
+            alert('Erro ao criar conta. Por favor, tente novamente.');
+        }
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        alert('Erro ao criar conta. Por favor, tente novamente.');
+    });
 });
