ce75296bc02839bfe8030bd4a697a925aed0b334
Cristian Massaneiro <cristianworc@gmail.com>
2024-10-18 01:13:17 -0300

    sdasdasd

--- sie/js/load-footer.js
+++ sie/js/load-footer.js
@@ -16,26 +16,17 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             footerElement.innerHTML = data;
 
-            // Após carregar o rodapé, ajustar os links e a imagem
-            var treineLink = footerElement.querySelector('a[href="treine.html"]');
-            var sobreLink = footerElement.querySelector('a[href="sobre.html"]');
-            var contatoLink = footerElement.querySelector('a[href="contato.html"]');
-            var footerLogo = footerElement.querySelector('.footer-logo-img');
+            // Após carregar o rodapé, ajustar os caminhos das imagens
+            var footerLogos = footerElement.querySelectorAll('.footer-logo-img');
 
-            // Ajusta links e o caminho da imagem de acordo com a localização (raiz ou subpasta)
-            if (isRoot) {
-                // Se estivermos na página index, ajustar links para a subpasta
-                treineLink.setAttribute('href', 'html/treine.html');
-                sobreLink.setAttribute('href', 'html/sobre.html');
-                contatoLink.setAttribute('href', 'html/contato.html');
-                footerLogo.setAttribute('src', './imgs/footer/logo_clouds_branco.png'); // Ajusta o caminho da logo
-            } else {
-                // Se estivermos dentro da subpasta 'html', ajustar links e imagem para a raiz
-                treineLink.setAttribute('href', '../treine.html');
-                sobreLink.setAttribute('href', '../sobre.html');
-                contatoLink.setAttribute('href', '../contato.html');
-                footerLogo.setAttribute('src', '../imgs/footer/logo_clouds_branco.png'); // Ajusta o caminho da logo
-            }
+            // Ajusta o caminho das imagens de acordo com a localização (raiz ou subpasta)
+            footerLogos.forEach(function(logo) {
+                if (isRoot) {
+                    logo.setAttribute('src', './imgs/footer/logo_clouds_branco.png'); // Ajusta o caminho da logo na raiz
+                } else {
+                    logo.setAttribute('src', '../imgs/footer/logo_clouds_branco.png'); // Ajusta o caminho da logo na subpasta
+                }
+            });
         })
         .catch(error => {
             console.error('Erro ao carregar o rodapé:', error);
